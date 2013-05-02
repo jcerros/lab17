cd324533213768e8b3bdd7e7359ab5b9ee30f2a6
jcerros <cerros_juan@yahoo.com>
2013-05-01 17:40:34 -0700

    Update and rename README.md to Testprogram.java

---
+++ Testprogram.java
@@ -0,0 +1,10 @@
+public class Testprogram {
+
+	public static void main(String args[]) {
+
+		System.out.println(Dice.Roll_1_Dice());
+		System.out.println(Dice.RollDice(2, 6));
+		System.out.println(Dice.RollDice(2, 6));
+		System.out.println(Dice.RollCount());
+	}
+}
